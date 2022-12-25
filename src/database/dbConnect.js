a8945037c90527d7fa0418c1ea27dc4cd7ee4602
Megas-MDN <mdn.melqui@gmail.com>
2022-12-25 01:00:31 -0300

    feat: Connect to DB mongoose

---
+++ src/database/dbConnect.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+
+export const connectDB = () => {
+  mongoose.set('strictQuery', true);
+  mongoose
+    .connect(process.env.CONNECT_DB)
+    .then(() => console.log('Connected DB'))
+    .catch((err) => console.log('Error to connect database', err));
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
+import { connectDB } from './database/dbConnect.js';
 config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json());
+connectDB();
+
 app.use(
   cors({
     origin: '*',
